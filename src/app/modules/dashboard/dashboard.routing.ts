ce4073b52d25a6fb8734246b5abf80fc78ff4063
Marcos Albano <marcosaalbanojunior@gmail.com>
2024-03-25 16:12:26 -0300

    configurando dashboard e rotas

---
+++ src/app/modules/dashboard/dashboard.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { DashboardHomeComponent } from "./page/dashboard-home/dashboard-home.component";
+
+export const DASHBOARD_ROUTE: Routes = [
+  {
+    path: '',
+    component: DashboardHomeComponent,
+  }
+]
