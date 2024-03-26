56f245341103b8408eac3f5ac23c818d70660fe3
Marcos Albano <marcosaalbanojunior@gmail.com>
2024-03-26 02:49:11 -0300

    Adicionando metodo delete

--- src/app/modules/products/components/products-table/products-table.component.ts
+++ src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,25 +1,36 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProductEvent } from 'src/app/models/enums/products/ProductEvent';
+import { DeleteProductAction } from 'src/app/models/interfaces/products/event/DeleteProductAction';
 import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 
 @Component({
   selector: 'app-products-table',
   templateUrl: './products-table.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class ProductsTableComponent {
-@Input() products: Array<GetAllProductsResponse> = [];
-@Output() productEvent = new EventEmitter<EventAction>();
+  @Input() products: Array<GetAllProductsResponse> = [];
+  @Output() productEvent = new EventEmitter<EventAction>();
+  @Output() deleteProductEvent = new EventEmitter<DeleteProductAction>();
 
-public productSelected!: GetAllProductsResponse;
-public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
-public editProductEvent = ProductEvent.EDIT_PRODUCT_EVENT;
+  public productSelected!: GetAllProductsResponse;
+  public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductEvent = ProductEvent.EDIT_PRODUCT_EVENT;
 
-handleProductEvent(action: string, id?: string): void{
-  if(action && action !== ''){
-    const ProductEventData = id && id!== ''? {action, id} : {action};
-    this.productEvent.emit(ProductEventData);
+  handleProductEvent(action: string, id?: string): void {
+    if (action && action !== '') {
+      const ProductEventData = id && id !== '' ? { action, id } : { action };
+      this.productEvent.emit(ProductEventData);
+    }
   }
-}
+
+  handleDeleteProduct(product_id: string, productName: string): void {
+    if (product_id !== '' && productName !== '') {
+      this.deleteProductEvent.emit({
+        product_id,
+        productName,
+      });
+    }
+  }
 }
--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,4 +1,4 @@
-import { MessageService } from 'primeng/api';
+import { MessageService, ConfirmationService } from 'primeng/api';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
@@ -21,7 +21,8 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
     private productsService: ProductsService,
     private productsDataTransferService: ProductsDataTransferService,
     private router: Router,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
   ) {}
 
   ngOnInit(): void {
@@ -60,9 +61,56 @@ export class ProductsHomeComponent implements OnInit, OnDestroy {
       });
   }
 
-  handleProductAction(event: EventAction): void{
-    if(event){
+  handleProductAction(event: EventAction): void {
+    if (event) {
       console.log('Dados do evento recebido', event);
+    }
+  }
+
+  handleDeleteProductAction(event: {
+    product_id: string;
+    productName: string;
+  }): void {
+    if (event) {
+      this.confirmationService.confirm({
+        message: `Confirma a exclusão do produto: ${event?.productName}`,
+        header: 'Confirmação de Exlucão',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => this.deleteProduct(event?.product_id),
+      });
+    }
+  }
+
+  deleteProduct(product_id: string) {
+    if (product_id) {
+      this.productsService
+        .deleteProduct(product_id)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Produto removido com sucesso',
+                life: 2500,
+              });
+
+              this.getAPIProductsData();
+            }
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao remover Produto',
+              life: 2500,
+            });
+          },
+        });
     }
   }
 
